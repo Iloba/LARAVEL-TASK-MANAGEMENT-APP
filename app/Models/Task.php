bf30fc4174c81116855fc38113d51e896909e312
Iloba <scarlettsney@gmail.com>
2021-07-15 01:32:38 +0100

    added-new-relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use App\Models\Project;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -21,5 +22,10 @@ class Task extends Model
     //relationship
     public function project(){
         return $this->belongsTo(Project::class);
+    }
+
+    //Relationship
+    public function user(){
+        return $this->belongsTo(User::class);
     }
 }
